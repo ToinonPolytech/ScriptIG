0e4e08cad5cb68fa0fb5c00ff5e7df7fa2e1325c
Kevin giordani <giordani.kevin.02@gmail.com>
2017-04-27 11:06:39 +0200

    Add files via upload

---
+++ invSouris.sh
@@ -0,0 +1,2 @@
+export DISPLAY=:0
+xinput set-int-prop "PixArt Dell MS116 USB Optical Mouse" "Evdev Axis Inversion" 8 0 1
---
+++ luminosite.sh
@@ -0,0 +1,5 @@
+while [ 1 ]
+do
+	xrandr --output HDMI2 --brightness 1
+	xrandr --output HDMI2 --brightness 0
+done
